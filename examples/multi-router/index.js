65cbd69b987e95bac15acc9cf349cef9fbad192e
Joey Guerra <github@joeyguerra.com>
2024-01-20 11:12:06 -0600

    feat: Remove the last use of setprototypeof
    BREAKING CHANGE: Router was previously a function, but now is an object. This API changes codde like `app.use(router)` to `app.use(router.handle.bind(router)`. Checkout the `test/Router.mjs` tests.

--- examples/multi-router/index.js
+++ examples/multi-router/index.js
@@ -3,9 +3,10 @@
 var express = require('../..');
 
 var app = module.exports = express();
-
-app.use('/api/v1', require('./controllers/api_v1'));
-app.use('/api/v2', require('./controllers/api_v2'));
+const api_v1 = require('./controllers/api_v1');
+const api_v2 = require('./controllers/api_v2');
+app.use('/api/v1', api_v1.handle.bind(api_v1));
+app.use('/api/v2', api_v2.handle.bind(api_v2));
 
 app.get('/', function(req, res) {
   res.send('Hello from root route.')
